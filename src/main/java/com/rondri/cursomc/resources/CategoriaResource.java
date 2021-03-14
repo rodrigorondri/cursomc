864e99ac8f1123ffa3aa4bcb838de16587c70295
Rodrigo R. Silveira <rondri@gmail.com>
2021-03-14 13:26:48 -0300

    Testanto classe de dominio

--- src/main/java/com/rondri/cursomc/resources/CategoriaResource.java
+++ src/main/java/com/rondri/cursomc/resources/CategoriaResource.java
@@ -1,15 +1,27 @@
 package com.rondri.cursomc.resources;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.rondri.cursomc.domain.Categoria;
 
 @RestController
 @RequestMapping(value="/Categorias")
 public class CategoriaResource {
 	
 	@RequestMapping(method=RequestMethod.GET)
-	public String listar() {
-		return "REST OK";
+	public List<Categoria> listar() {
+		Categoria cat1 = new Categoria(1,"Informáticoa");
+		Categoria cat2 = new Categoria(2,"Escritório");
+		
+		List<Categoria> lista = new ArrayList<>();
+		lista.add(cat1);
+		lista.add(cat2);
+		
+		return lista;
 	}
 }
