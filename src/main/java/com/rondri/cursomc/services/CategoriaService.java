2017c14539219a3437817b10ca69a663febf862f
Rodrigo R. Silveira <rondri@gmail.com>
2021-04-11 14:19:52 -0300

    Ajustes no edpoint /Categorias/{id}

--- src/main/java/com/rondri/cursomc/services/CategoriaService.java
+++ src/main/java/com/rondri/cursomc/services/CategoriaService.java
@@ -7,6 +7,7 @@ import org.springframework.stereotype.Service;
 
 import com.rondri.cursomc.domain.Categoria;
 import com.rondri.cursomc.repositories.CategoriaRepository;
+import com.rondri.cursomc.services.exceptions.ObjectNotFoundException;
 
 @Service
 public class CategoriaService {
@@ -14,9 +15,9 @@ public class CategoriaService {
 	@Autowired
 	private CategoriaRepository repo;
 
-	public Categoria find(Integer id){
+	public Categoria find(Integer id) {
 		Optional<Categoria> obj = repo.findById(id);
-		return obj.orElse(null);
-
-		}
+		return obj.orElseThrow(() -> new ObjectNotFoundException(
+				"Objeto não encontrado! Id: " + id + ", Tipo: " + Categoria.class.getName()));
+	}
 }
