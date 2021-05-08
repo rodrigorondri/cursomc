93a7e6e26735fc772cd363b20d7a99e03dc8557e
Rodrigo R. Silveira <rondri@gmail.com>
2021-05-08 11:47:50 -0300

    Correcao update em CategoriaService e construtor de Pagamento

--- src/main/java/com/rondri/cursomc/services/CategoriaService.java
+++ src/main/java/com/rondri/cursomc/services/CategoriaService.java
@@ -33,10 +33,13 @@ public class CategoriaService {
 		return repo.save(obj);
 	}
 	
+	
 	public Categoria update(Categoria obj) {
-		find(obj.getId());
-		return repo.save(obj);
+		Categoria newObj =  find(obj.getId());
+		updateData(newObj,obj);
+		return repo.save(newObj);
 	}
+	
 	
 	public void delete(Integer id) {
 		find(id);
@@ -60,6 +63,10 @@ public class CategoriaService {
 	
 	public Categoria fromDTO(CategoriaDTO objDTO) {
 		return new Categoria(objDTO.getId(),objDTO.getNome());
+	}
+	
+	private void updateData(Categoria newObj,Categoria obj) {
+		newObj.setNome(obj.getNome());
 	}
 }
 	
