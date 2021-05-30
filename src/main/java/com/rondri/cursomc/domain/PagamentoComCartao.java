e38211a61908eabffe67b97d899604a90bfab27d
Rodrigo R. Silveira <rondri@gmail.com>
2021-05-30 13:57:59 -0300

    Inserindo Pedido

--- src/main/java/com/rondri/cursomc/domain/PagamentoComCartao.java
+++ src/main/java/com/rondri/cursomc/domain/PagamentoComCartao.java
@@ -2,9 +2,11 @@ package com.rondri.cursomc.domain;
 
 import javax.persistence.Entity;
 
+import com.fasterxml.jackson.annotation.JsonTypeName;
 import com.rondri.cursomc.domain.enums.EstadoPagamento;
 
 @Entity
+@JsonTypeName("pagamentoComCartao")
 public class PagamentoComCartao extends Pagamento{
 
   private static final long serialVersionUID = 1L;
--- src/main/java/com/rondri/cursomc/services/PedidoService.java
+++ src/main/java/com/rondri/cursomc/services/PedidoService.java
@@ -1,11 +1,19 @@
 package com.rondri.cursomc.services;
 
+import java.util.Date;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
+import com.rondri.cursomc.domain.ItemPedido;
+import com.rondri.cursomc.domain.PagamentoComBoleto;
 import com.rondri.cursomc.domain.Pedido;
+import com.rondri.cursomc.domain.Produto;
+import com.rondri.cursomc.domain.enums.EstadoPagamento;
+import com.rondri.cursomc.repositories.ItemPedidoRepository;
+import com.rondri.cursomc.repositories.PagamentoRepository;
 import com.rondri.cursomc.repositories.PedidoRepository;
 import com.rondri.cursomc.services.exceptions.ObjectNotFoundException;
 
@@ -14,10 +22,46 @@ public class PedidoService {
 	
 	@Autowired
 	private PedidoRepository repo;
+	
+	@Autowired
+	private BoletoService boletoService;
+	
+	@Autowired
+	private PagamentoRepository pagamentoRepository;
+	
+	@Autowired
+	private ProdutoService produtoService;
+	
+	@Autowired
+	private ItemPedidoRepository itemPedidoRepository;
 
 	public Pedido find(Integer id) {
 		Optional<Pedido> obj = repo.findById(id);
 		return obj.orElseThrow(() -> new ObjectNotFoundException(
 				"Objeto não encontrado! Id: " + id + ", Tipo: " + Pedido.class.getName()));
+	}
+	
+	@Transactional
+	public Pedido insert(Pedido obj) {
+		obj.setId(null);
+		obj.setInstante(new Date());
+		obj.getPagamento().setEstado(EstadoPagamento.PENDENDETE);
+		obj.getPagamento().setPedido(obj);
+		if(obj.getPagamento() instanceof PagamentoComBoleto) {
+			PagamentoComBoleto pagto = (PagamentoComBoleto) obj.getPagamento();
+			boletoService.preencherPagamentoComBoleto(pagto,obj.getInstante());
+		}
+		obj = repo.save(obj);
+		pagamentoRepository.save(obj.getPagamento());
+		for(ItemPedido ip : obj.getItens() ) {
+			ip.setDesconto(0.0);
+			//ip.setPreco(produtoService.findOne(ip.getProduto().getId()));
+			Produto prod = produtoService.find(ip.getProduto().getId());
+			ip.setPreco(prod.getPreco());
+			ip.setPedido(obj);
+		}
+		
+		itemPedidoRepository.saveAll(obj.getItens());
+		return obj;
 	}
 }
